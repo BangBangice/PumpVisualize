83237e0c8248a6b5f3cd59c5ea2a4ee414e76c6e
lee <sgghlee@gmail.com>
2022-06-15 22:08:08 +0800

    Initial commit

---
+++ src/main/java/com/jnu/pumpvisualize/PumpVisualizeApplication.java
@@ -0,0 +1,13 @@
+package com.jnu.pumpvisualize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpVisualizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumpVisualizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jnu/pumpvisualize/PumpVisualizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnu.pumpvisualize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpVisualizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
